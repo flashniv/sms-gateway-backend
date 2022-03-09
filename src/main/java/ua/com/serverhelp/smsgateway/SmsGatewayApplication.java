b2beadf3b1ac76d1b4fa9db9bf027df63ee158ea
flashniv <flashniv@gmail.com>
2022-03-09 15:27:11 +0200

    add message APIReference.yaml

--- src/main/java/ua/com/serverhelp/smsgateway/SmsGatewayApplication.java
+++ src/main/java/ua/com/serverhelp/smsgateway/SmsGatewayApplication.java
@@ -1,13 +1,17 @@
 package ua.com.serverhelp.smsgateway;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SmsGatewayApplication {
+public class SmsGatewayApplication implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SmsGatewayApplication.class, args);
 	}
-
 }
