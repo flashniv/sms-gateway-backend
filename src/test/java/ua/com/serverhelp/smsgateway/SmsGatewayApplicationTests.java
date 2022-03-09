fe3348a67f6e601e83bc13bd06ad427e825ed33c
flashniv <flashniv@gmail.com>
2022-03-09 14:53:57 +0200

    init

---
+++ src/test/java/ua/com/serverhelp/smsgateway/SmsGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.com.serverhelp.smsgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
